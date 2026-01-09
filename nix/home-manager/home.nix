1e95659bab75c7f34bdd33dbcbcfa0b75257be38
Randall Bennett <randall@boltfoundry.com>
2026-01-09 15:44:29 -0500

    Enable zsh integration for direnv

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -4,6 +4,7 @@
     enable = true;
     nix-direnv.enable = true;
   };
+  programs.zsh.enable = true;
   home.sessionPath = [ "$HOME/.local/bin" "/opt/homebrew/bin" "/usr/local/bin" ];
   home.stateVersion = "24.05";
 }
